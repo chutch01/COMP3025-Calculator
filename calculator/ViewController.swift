c473df97fc5dc4df5c8c80d1dd012d466b63b962
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 11:09:32 -0500

    added empty functions for each button to start coding

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,19 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+
+    @IBAction func ACButton(sender: UIButton) {
+    }
+    
+    @IBAction func PosNegButton(sender: UIButton){
+    }
+    
+    @IBAction func PercentButton(sender: UIButton){
+    }
+    
+    @IBAction func DivideButton(sender: UIButton) {
+    }
+    
+    @IBAction func SevenButton(sender: UIButton) {
+    }
+    
+    @IBAction func EightButton(sender: UIButton) {
+    }
+    
+    @IBAction func NineButton(sender: UIButton) {
+    }
+    
+    @IBAction func MultiplyButton(sender: UIButton){
+    }
+    @IBAction func FourButton(sender: UIButton) {
+    }
+    
+    @IBAction func FiveButton(sender: UIButton) {
+    }
+    
+    @IBAction func SixButton(sender: UIButton) {
+    }
+    
+    @IBAction func MinusButton(sender: UIButton) {
+    }
+    
+    @IBAction func OneButton(sender: UIButton) {
+    }
+    
+    @IBAction func TwoButton(sender: UIButton) {
+    }
+    
+    @IBAction func ThreeButton(sender: UIButton) {
+    }
+    
+    @IBAction func PlusButton(sender: UIButton) {
+    }
+    
+    @IBAction func ZeroButton(sender: UIButton) {
+    }
+    
+    @IBAction func DecimalButton(sender: UIButton) {
+    }
+    
+    @IBAction func EqualsButton(sender: UIButton) {
+    }
     
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
