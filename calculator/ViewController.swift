2990d511c2466ae8a6b5211e5a533fca35e030a4
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 11:57:12 -0500

    wrote up button functions, still need to work on calculate

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,62 +9,226 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //constraints
+    var result = "0"
+    var negPos = ""
+    var firstNum = 0.0
+    var secondNum = 0.0
+    var total = 0.0
+    var isFirst = true
+    var mathType = -1
+    var reset = false
+    
+    
+    //constants
+    let ADD = 0
+    let SUB = 1
+    let MUL = 2
+    let DIV = 3
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
+    
+    //protected methods
+    
+    func updateLabel(){
+        resultLabel.text = negPos + result
+    }
+    
+    func stringToNum(){
+        firstNum = (result as NSString).doubleValue
+        
+        if(negPos == "-"){
+            firstNum *= -1
+            negPos = ""
+            
+            updateLabel()
+        }
+        else{
+            secondNum = (result as NSString).doubleValue
+            
+            if(negPos == "-"){
+                secondNum *= -1
+                negPos = ""
+                
+            }
+            isFirst = false
+            result = "0"
+        }
+        
+    }
+    
+    
+    //button methods
 
     @IBAction func ACButton(sender: UIButton) {
+        result = "0"
+        firstNum = 0
+        secondNum = 0
+        isFirst = true
+        negPos = ""
     }
     
     @IBAction func PosNegButton(sender: UIButton){
+        
+        if (negPos == ""){
+            negPos = "-"
+            
+        }
+        else{
+            negPos = ""
+        }
+        updateLabel()
     }
     
     @IBAction func PercentButton(sender: UIButton){
+        if(!isFirst && firstNum != 0){
+            stringToNum()
+            secondNum /= 100
+            secondNum *= firstNum
+            result = String(secondNum)
+            updateLabel()
+        }
     }
     
     @IBAction func DivideButton(sender: UIButton) {
+        if(isFirst){
+            stringToNum()
+        }
+        mathType = DIV
     }
-    
-    @IBAction func SevenButton(sender: UIButton) {
+    @IBAction func PlusButton(sender: UIButton) {
+        if(isFirst){
+            stringToNum()
+        }
+        mathType = ADD
     }
-    
-    @IBAction func EightButton(sender: UIButton) {
-    }
-    
-    @IBAction func NineButton(sender: UIButton) {
-    }
-    
     @IBAction func MultiplyButton(sender: UIButton){
+        if(isFirst){
+            stringToNum()
+        }
+        mathType = MUL
     }
-    @IBAction func FourButton(sender: UIButton) {
-    }
-    
-    @IBAction func FiveButton(sender: UIButton) {
-    }
-    
-    @IBAction func SixButton(sender: UIButton) {
-    }
-    
     @IBAction func MinusButton(sender: UIButton) {
+        if(isFirst){
+            stringToNum()
+        }
+        mathType = SUB
     }
-    
     @IBAction func OneButton(sender: UIButton) {
+        if(result == "0"){
+            result = "1"
+        }
+        else{
+            result += "1"
+        }
+        updateLabel()
     }
     
     @IBAction func TwoButton(sender: UIButton) {
+        if(result == "0"){
+            result = "2"
+        }
+        else{
+            result += "2"
+        }
+        updateLabel()
     }
     
     @IBAction func ThreeButton(sender: UIButton) {
+        if(result == "0"){
+            result = "3"
+        }
+        else{
+            result += "3"
+        }
+        updateLabel()
     }
     
-    @IBAction func PlusButton(sender: UIButton) {
+    @IBAction func FourButton(sender: UIButton) {
+        if(result == "0"){
+            result = "4"
+        }
+        else{
+            result += "4"
+        }
+        updateLabel()
     }
+    
+    @IBAction func FiveButton(sender: UIButton) {
+        if(result == "0"){
+            result = "5"
+        }
+        else{
+            result += "5"
+        }
+        updateLabel()
+    }
+    
+    @IBAction func SixButton(sender: UIButton) {
+        if(result == "0"){
+            result = "6"
+        }
+        else{
+            result += "6"
+        }
+        updateLabel()
+    }
+
+    @IBAction func SevenButton(sender: UIButton) {
+        if(result == "0"){
+            result = "7"
+        }
+        else{
+            result += "7"
+        }
+        updateLabel()
+    }
+    
+    @IBAction func EightButton(sender: UIButton) {
+        if(result == "0"){
+            result = "8"
+        }
+        else{
+            result += "8"
+        }
+        updateLabel()
+    }
+    
+    @IBAction func NineButton(sender: UIButton) {
+        if(result == "0"){
+            result = "9"
+        }
+        else{
+            result += "9"
+        }
+        updateLabel()
+    }
+    
     
     @IBAction func ZeroButton(sender: UIButton) {
+        if(result != "0"){
+            result += "0"
+            updateLabel()
+        }
+      
+        
     }
     
     @IBAction func DecimalButton(sender: UIButton) {
+        if(result.rangeOfString(".")==nil){
+            result += "."
+            updateLabel()
+        }
+      
+        
     }
     
+    
     @IBAction func EqualsButton(sender: UIButton) {
+        
     }
     
     
